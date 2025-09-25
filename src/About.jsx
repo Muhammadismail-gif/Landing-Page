5baff14a9a99ea1ba181cf0510f620ab2ef8dd28
Muhammad Ismail <mi.ismail000@gmail.com>
2025-09-25 12:37:38 +0500

    Add Responsiveness and some icons shadows

--- src/About.jsx
+++ src/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
         <div className="about-right">
             <h1>ABOUT US</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-            <p className='para2'><i class="fa-solid fa-star"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
-            <p className='para2'><i class="fa-solid fa-star"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            <p className='para2'><i >☆</i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            <p className='para2'><i >☆</i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
             <h3>Why choose Us</h3> <br />
-            <pre>Lorem ipsum dolor sit          Lorem ipsum dolor sit.</pre> <br />
-            <pre>Lorem ipsum dolor sit          Lorem ipsum dolor sit.</pre> <br />
-            <pre>Lorem ipsum dolor sit          Lorem ipsum dolor sit.</pre> <br />
-            <pre>Lorem ipsum dolor sit          Lorem ipsum dolor sit.</pre> <br />
+            <pre> ✔ Lorem ipsum dolor sit         ✔ Lorem ipsum dolor sit.</pre> <br />
+            <pre> ✔ Lorem ipsum dolor sit         ✔ Lorem ipsum dolor sit.</pre> <br />
+            <pre> ✔ Lorem ipsum dolor sit         ✔ Lorem ipsum dolor sit.</pre> <br />
+            <pre> ✔ Lorem ipsum dolor sit         ✔ Lorem ipsum dolor sit.</pre> <br />
         </div>
       </div>
     </div>
